84f130ac29f86e3a36a3c1ac0195feb1d4ab07b6
Hasaan Keen <Hasaankeen1@gmail.com>
2025-09-09 14:50:11 -0400

    implemented java method and test files raindrops.

---
+++ src/main/java/RaindropConverter.java
@@ -0,0 +1,25 @@
+class RaindropConverter {
+
+    String convert(int number) {
+
+        StringBuilder result = new StringBuilder();
+
+        if (number % 3 == 0) {
+            result.append("Pling");
+        }
+
+        if (number % 5 == 0) {
+            result.append("Plang");
+        }
+
+        if (number % 7 == 0) {
+            result.append("Plong");
+        }
+
+        if (result.length() == 0) {
+            return String.valueOf(number);
+        } else {
+            return result.toString();
+        }
+    }
+}
